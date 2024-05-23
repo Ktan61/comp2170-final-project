8eb73fb7b79ee86d98f9461555cf89c4e1aa96cd
KathrynTan <ktan61@my.bcit.ca>
2024-05-22 21:31:38 -0700

    Minor edits

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // MENU SCROLL BUTTONS
 document.addEventListener("DOMContentLoaded", function() {
-    // Function to handle the scrolling
     const handleScroll = (elementId) => {
         document.getElementById(elementId).scrollIntoView({ behavior: 'smooth' });
     };
 
-    // Attach event listeners to buttons
     document.querySelectorAll('[data-scroll-target]').forEach(button => {
         button.addEventListener('click', function() {
             const targetId = this.getAttribute('data-scroll-target');
